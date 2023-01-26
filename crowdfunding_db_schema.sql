62a44571939692df3f58f8aa34f263b5a738bfcc
Oseremhen <106120403+Oseremhen@users.noreply.github.com>
2023-01-26 18:40:56 -0500

    Updates

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,49 +1,49 @@
 --Drop tables if they already exist
+DROP TABLE IF EXISTS campaign;
 DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS subcategory;
-DROP TABLE IF EXISTS campaign;
 DROP TABLE IF EXISTS contacts;
-
 
 --Create all the tables
 CREATE TABLE category(
-	category_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	category_id VARCHAR(15) PRIMARY KEY NOT NULL,
 	category VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE subcategory(
-	subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	subcategory_id VARCHAR(15) PRIMARY KEY NOT NULL,
 	subcategory VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE contacts(
-	contact_id INTEGER PRIMARY KEY,
+	contact_id INTEGER PRIMARY KEY NOT NULL,
 	first_name VARCHAR (20) NOT NULL,
 	last_name VARCHAR (20) NOT NULL,
-	email VARCHAR (60) NOT NULL
+	email VARCHAR (45) NOT NULL
 );
 
 CREATE TABLE campaign(
 	cf_id INTEGER PRIMARY KEY NOT NULL,
-	contact_id INTEGER,
-	company_name VARCHAR(50),
+	contact_id INTEGER NOT NULL,
+	company_name VARCHAR(50) NOT NULL,
 	description VARCHAR NOT NULL,
-	goal FLOAT,
-	pledged FLOAT,
+	goal DECIMAL NOT NULL,
+	pledged DECIMAL NOT NULL,
 	outcome VARCHAR (15) NOT NULL,
-	backers_count INTEGER,
-	country VARCHAR (10) NOT NULL,
-	currency VARCHAR (10) NOT NULL,
-	launched_date DATE,
-	end_date DATE,
-	category_id VARCHAR (10) NOT NULL,
-	subcategory_id VARCHAR (10) NOT NULL,
+	backers_count INTEGER NOT NULL,
+	country VARCHAR (5) NOT NULL,
+	currency VARCHAR (5) NOT NULL,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR (15) NOT NULL,
+	subcategory_id VARCHAR (15) NOT NULL,
 	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
 	FOREIGN KEY (category_id) REFERENCES category(category_id),
 	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
 );
 
---Check created tables and After importing the data, show the tables
+--Check created tables with the code below
+--After importing the data, show the tables with the code below
 SELECT * FROM category;
 SELECT * FROM subcategory;
 SELECT * FROM contacts;
